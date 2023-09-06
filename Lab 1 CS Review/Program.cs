27b08d2b3fc1b114155ae5690a3d8d942bfa1672
IsaacRoss727 <113382738+IsaacRoss727@users.noreply.github.com>
2023-09-06 19:27:19 -0400

    Add files via upload

---
+++ Lab 1 CS Review/Program.cs
@@ -0,0 +1,112 @@
+﻿using System.Reflection.Metadata;
+
+namespace Lab_1_CS_Review
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string projectFolder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString();
+            string dataPath = projectFolder + Path.DirectorySeparatorChar + "videogames.csv";
+
+            List<VideoGame> games = new List<VideoGame>();
+
+            using (var reader = new StreamReader(dataPath))
+            {
+                string fileHeader = reader.ReadLine();
+                while(!reader.EndOfStream)
+                {
+                    string? line = reader.ReadLine();
+                    string[] lineElements = line.Split(',');
+
+                    VideoGame g = new VideoGame()
+                    {
+                        Name = lineElements[0],
+                        Platform = lineElements[1],
+                        Year = lineElements[2],
+                        Genre = lineElements[3].ToLower(),
+                        Publisher = lineElements[4].ToLower(),
+                        NA_Sales = lineElements[5],
+                        EU_Sales = lineElements[6],
+                        JP_Sales = lineElements[7],
+                        Other_Sales = lineElements[8],
+                        Global_Sales = lineElements[9]
+                    };
+                    games.Add(g);
+                }
+
+                games.Sort();
+
+                var orderedGameByTitle = games.OrderBy(g => g.Name);
+                foreach (var game in orderedGameByTitle)
+                {
+                    Console.WriteLine(game);
+                }
+
+                var gamesPublishedByCapcom = games.Where(g => g.Publisher == "Capcom"); //Step 3
+                foreach (var game in gamesPublishedByCapcom)
+                {
+                    Console.WriteLine(game);
+                }
+
+                static void PublisherData(List<VideoGame> game, string publisher) //Method to do step 4 and 7
+                //While writing this I realized it works for step 7 so I renamed it accordingly
+                {
+                    float numGames = game.Count;
+                    var publisherGames = new List<VideoGame>();
+                    foreach (var g in game)
+                    {
+                        if(g.Publisher == publisher)
+                            publisherGames.Add(g);
+                    }
+                    float numGamesByPublisher = publisherGames.Count;
+                    var percentOfGamesByPublisher = (numGamesByPublisher / numGames) * 100;
+
+                    Console.WriteLine($"Out of {numGames}, {numGamesByPublisher} are made by {publisher}, which is {percentOfGamesByPublisher:0.##}%");
+
+                }
+                var fightingGames = games.Where(g => g.Genre == "Fighting"); //Step 5
+                foreach (var game in fightingGames)
+                {
+                    Console.WriteLine(game);
+                }
+
+                PublisherData(games, "capcom"); //Actually calling on the step 4 method
+
+                static void GenreData(List<VideoGame> game, string genre) //Method to do step 6, reused from step 4
+                {
+                //Had the same epiphany here as with the PublisherData method.
+                    float numGames = game.Count;
+                    var genreGames = new List<VideoGame>();
+                    foreach (var g in game)
+                    {
+                        if (g.Genre == genre)
+                            genreGames.Add(g);
+                    }
+                    float numGamesInGenre = genreGames.Count;
+                    var percentOfGamesInGenre = (numGamesInGenre / numGames) * 100;
+
+                    Console.WriteLine($"Out of {numGames}, {numGamesInGenre} are in the {genre} genre, which is {percentOfGamesInGenre:0.##}%");
+
+                }
+
+                GenreData(games, "fighting"); //Using the step 6 method
+
+                Console.WriteLine("What publisher's data would you like to check?");
+                string userChoiceOfPublisher = Console.ReadLine().ToLower();
+                
+                PublisherData(games, userChoiceOfPublisher);
+
+                Console.WriteLine();
+                Console.WriteLine("What genre's data would you like to check?");
+                string userChoiceOfGenre = Console.ReadLine().ToLower();
+
+                GenreData(games, userChoiceOfGenre);
+
+
+
+
+            }
+        }
+    }
+}
---
+++ Lab 1 CS Review/VideoGame.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_CS_Review
+{
+    internal class VideoGame : IComparable<VideoGame> //Step 2
+    { //step 1
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public string Year { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public string NA_Sales { get; set; }
+        public string EU_Sales { get; set; }
+        public string JP_Sales { get; set; }
+        public string Other_Sales { get; set; }
+        public string Global_Sales { get; set; }
+
+        public VideoGame() { }
+
+        public int CompareTo(VideoGame? other)
+        {
+            return Name.CompareTo(other.Name);
+        }
+
+        public override string ToString() //Step 2
+        {
+            string videoGameString = "";
+            videoGameString += $"Title: {Name}\n";
+            videoGameString += $"Platform: {Platform}\n";
+            videoGameString += $"Year: {Year}\n";
+            videoGameString += $"Genre: {Genre}\n\n";
+            videoGameString += $"Publisher: {Publisher}\n\n";
+            videoGameString += $"NA Sales: {NA_Sales}\n";
+            videoGameString += $"EU Sales: {EU_Sales}\n";
+            videoGameString += $"JP Sales: {JP_Sales}\n";
+            videoGameString += $"Other Sales: {Other_Sales}\n";
+            videoGameString += $"Total Sales: {Global_Sales}\n";
+
+            videoGameString += $"---------------------------------------------------------------";
+            return videoGameString;
+        }
+    }
+}
